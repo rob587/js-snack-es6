346d7e8e276cafc70c50b6bb893588233a002247
rob587 <robertocamm02@yahoo.com>
2025-07-14 21:25:31 +0200

    object created

--- js/script.js
+++ js/script.js
@@ -43,3 +43,41 @@ for(let i=1; i<bicycle.length; i++){
 }
 
 console.log(" la bicicletta col peso minore è:", lightbicycle.name, "con", lightbicycle.weight, "kg" )
+
+
+//-----------------------//
+
+// Snack2**
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squads = [
+    {   
+        nome : "Milan",
+        points: 0,
+        fouls: 0
+    },
+    {   
+        nome : "Juve",
+        points: 0,
+        fouls: 0
+    },
+    {   
+        nome : "Inter",
+        points: 0,
+        fouls: 0
+    },
+    {   
+        nome : "Napoli",
+        points: 0,
+        fouls: 0
+    },
+    {   
+        nome : "Psg",
+        points: 0,
+        fouls: 0
+    }
+]
