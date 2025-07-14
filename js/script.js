ccf0337a057df21fbd7c88ebe1b30c1eea027df5
rob587 <robertocamm02@yahoo.com>
2025-07-14 15:47:27 +0200

    objects

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+
+const bicycle = [
+    {
+        name: 'Ugo',
+        weight: 30,
+        year:2006
+    },
+    {
+        name: 'Ugo',
+        weight: 30,
+        year:2006
+    },
+    {
+        name: 'Ugo',
+        weight: 30,
+        year:2006
+    },
+    {
+        name: 'Ugo',
+        weight: 30,
+        year:2006
+    },
+    {
+        name: 'Ugo',
+        weight: 30,
+        year:2006
+    }
+]
